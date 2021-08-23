211046cc0e4f3b24df008ceaa679b613b8cdeec8
MAHENDHAR APPALA <appalamahendhar138@gmail.com>
2021-08-23 22:41:35 +0530

    capstone project

---
+++ mernstack/server/config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGOURI : "mongodb+srv://mahendhar:sKx4SKaKTRTo3YZJ@cluster0.bxhot.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    JWT_SECRET : "mahendhar123456"
+}
